23544ebfa787096dddaebbf4fe7d49079bf2a067
Urmila-m <urmi.mhrz@gmail.com>
2021-05-20 20:17:28 +0545

    Code refactoring

--- js/index.js
+++ js/index.js
@@ -124,7 +124,8 @@ class Game{
     }  
 
     mouseUpHandler(e){
-        this.swapCandies(this.move);
+        new SwapCandy(this.selectedCandy, this.candiesArray).swapCandies(this.move);
+        // this.swapCandies(this.move);
         this.isDragging = false;
         this.selectedCandy = null;
         this.offsetX = 0;
@@ -160,61 +161,6 @@ class Game{
             }
         }
     }
-
-    swapCandies(move){
-        if(move !== DONT_MOVE){
-            let swapCandyRow = this.selectedCandy.row;
-            let swapCandyColumn = this.selectedCandy.column;
-            let candyToBeSwapped;
-
-            if(move === MOVE_LEFT){
-                swapCandyColumn = this.selectedCandy.column - 1;
-                candyToBeSwapped = this.placeSwappingCandy(swapCandyRow, swapCandyColumn);
-                candyToBeSwapped.column += 1;
-                this.selectedCandy.column -= 1;
-            
-            }
-            else if(move === MOVE_RIGHT){
-                swapCandyColumn = this.selectedCandy.column + 1;
-                candyToBeSwapped = this.placeSwappingCandy(swapCandyRow, swapCandyColumn);
-                candyToBeSwapped.column -= 1;
-                this.selectedCandy.column += 1;
-
-            }
-
-            else if(move === MOVE_UP){
-                swapCandyRow = this.selectedCandy.row +1;
-                candyToBeSwapped = this.placeSwappingCandy(swapCandyRow, swapCandyColumn);
-                candyToBeSwapped.row -= 1;
-                this.selectedCandy.row +=1;
-            }
-
-            else if(move === MOVE_DOWN){
-                swapCandyRow = this.selectedCandy.row - 1;
-                candyToBeSwapped = this.placeSwappingCandy(swapCandyRow, swapCandyColumn);
-                candyToBeSwapped.row += 1;
-                this.selectedCandy.row -=1;
-            }
-            
-            this.resetSwappedCandiesPosition(candyToBeSwapped, swapCandyRow, swapCandyColumn);
-        }
-        else{
-            this.selectedCandy.resetPosition();
-        }
-    }
-
-    resetSwappedCandiesPosition(candyToBeSwapped, swapCandyRow, swapCandyColumn){
-        this.candiesArray[swapCandyColumn][swapCandyRow] = this.selectedCandy; 
-        this.selectedCandy.resetPosition();
-        candyToBeSwapped.resetPosition();
-    }
-
-    placeSwappingCandy(swapCandyRow, swapCandyColumn){
-        let candyToBeSwapped = this.candiesArray[swapCandyColumn][swapCandyRow];
-        this.candiesArray[this.selectedCandy.column][this.selectedCandy.row] = candyToBeSwapped;
-        return candyToBeSwapped;
-    }
-
 }
 
 new Game();
